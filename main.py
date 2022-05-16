3d3ab6836c6adcfecd20786caba40f013c6d3c3b
matteolanglois <langlois.matteo49@gmail.com>
2022-05-16 14:31:39 +0200

    Add ram usage, embed and command

--- main.py
+++ main.py
@@ -2,25 +2,46 @@ import discord
 from discord.ext import commands
 import psutil
 import matplotlib.pyplot as plt
-from PIL import Image
+import csv
+import datetime as datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from threading import Thread
 
 bot = commands.Bot(command_prefix='$')
+
+scheduler = BackgroundScheduler()
+
+def TempGathering():
+    print("a")
+    temp = psutil.sensors_temperatures()
+    core = [temp['coretemp'][I][1] for I in range(0, 6)]
+    core.append(psutil.cpu_percent() * 10)
+    core.insert(0, datetime.datetime.now())
+    with open('CPUtemp.csv', 'a', newline='') as Temp:
+        writer = csv.writer(Temp)
+        writer.writerow(core)
+        print(core)
+        Temp.close()
+
+def SchCPU():
+    scheduler.add_job(TempGathering, 'interval', minutes=0.1, replace_existing=True, max_instances=20)
 
 @bot.event
 async def on_ready():
     print("Logged on as", bot.user)
 
+
 async def on_message(message):
     if message.author.id == bot.user.id:
         return
-    if "di" in message.content.lower():
+    elif "di" in message.content.lower():
         li = message.content.split(" ")
         for word in li:
             if "di" in word.lower():
                 await message.channel.send(word[2:])
-    if "quoi" in message.content.lower():
+    elif "quoi" in message.content.lower():
         li = message.content.split(" ")
-        li = [I.lower for I in li]
+        li = [I.lower() for I in li]
         pos = li.index("quoi")
         for I in range(pos, len(li)):
             if I in ["?", ":", "!", "."]:
@@ -31,9 +52,10 @@ async def on_message(message):
 async def ping(ctx):
     await ctx.channel.send("pong")
 
-@bot.command(name="say", help='Responds with the args')
+@bot.command(name="say", help='Responds with the args and delete the command')
 async def text(ctx, arg):
     await ctx.send(arg)
+    await ctx.message.delete()
 
 @bot.command(help='Responds with the temp of the CPU of your computer who host the bot')
 async def cpu(ctx):
@@ -73,7 +95,29 @@ async def ram(ctx):
     plt.pie([psutil.virtual_memory().available, psutil.virtual_memory().total - psutil.virtual_memory().available],
             labels=["RAM Disponible", "RAM Utilisée"])
     plt.savefig("/var/www/html/temp/temp.png",  bbox_inches='tight')
+
     embedvar.set_image(url="https://les-roseaux.dev/temp/temp.png")
     await ctx.send(embed=embedvar)
+
+@bot.command(help='Responds with graph of CPU Stats')
+async def CPU_usage(ctx):
+    embedvar = discord.Embed(
+        title="CPU stats", color=0xff4030
+    )
+    stats = [[], [], [], [], [], [], [], []]
+    with open('CPUtemp.csv', 'r', newline='') as Temp:
+        file_reader = csv.reader(Temp)
+        for line in file_reader:
+            for I in range(len(line)):
+                stats[I].append(float(line[I]))
+    for stat in stats[1:6]:
+        print(len(stat), len(stats[0]))
+        plt.plot(stat, stats[0])
+    plt.savefig("/var/www/html/temp/temp2.png", bbox_inches='tight')
+    embedvar.set_image(url="https://les-roseaux.dev/temp/temp2.png")
+    await ctx.send(embed=embedvar)
+
+CPU_temp = Thread(target=SchCPU)
+CPU_temp.start()
 
 bot.run("OTc0OTQ3Nzc0Nzg3MzY3MDEz.GJQ8hB.kZ4xt9ky5oTcEIXBfze4aAiv2EsDe8ubi6rKvQ")
